be5158faf826997e6bb2c8d235533b83b48dc5c4
songihyeon <tyumr70@gmail.com>
2025-04-04 15:32:27 +0900

    task completed

--- contracts/AbstractCalculator.sol
+++ contracts/AbstractCalculator.sol
@@ -1,1 +1,28 @@
-abstract contract AbstractCalculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ICalculator.sol";
+import "./MathLibrary.sol";
+
+abstract contract AbstractCalculator is ICalculator {
+    using MathLibrary for uint256;
+
+    function calculate(
+        uint a,
+        uint b,
+        string memory operation
+    ) public pure virtual returns (uint) {}
+
+    function add(uint a, uint b) public pure override returns (uint) {
+        return MathLibrary.add(a, b);
+    }
+    function subtract(uint a, uint b) public pure override returns (uint) {
+        return MathLibrary.subtract(a, b);
+    }
+    function multiply(uint a, uint b) public pure override returns (uint) {
+        return MathLibrary.multiply(a, b);
+    }
+    function divide(uint a, uint b) public pure override returns (uint) {
+        return MathLibrary.divide(a, b);
+    }
+}
--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,1 +1,36 @@
-contract Calculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AbstractCalculator.sol";
+
+contract Calculator is AbstractCalculator {
+    function calculate(
+        uint a,
+        uint b,
+        string memory operation
+    ) public pure override returns (uint) {
+        if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("add"))
+        ) {
+            return add(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("subtract"))
+        ) {
+            return subtract(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("multiply"))
+        ) {
+            return multiply(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("divide"))
+        ) {
+            return divide(a, b);
+        } else {
+            revert("Invalid operation");
+        }
+    }
+}
